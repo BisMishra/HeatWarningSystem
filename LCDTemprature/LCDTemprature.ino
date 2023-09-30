832f446b218583cdf01d15c2273f98469b9f4fef
MJRakhra <86208038+MJRakhra@users.noreply.github.com>
2023-09-30 14:44:38 -0500

    Add files via upload
    Code for taking input data from Temperature sensor in A0

---
+++ LCDTemprature/LCDTemprature.ino
@@ -0,0 +1,39 @@
+/*
+  ReadAnalogVoltage
+
+  Reads an analog input on pin 0, converts it to voltage, and prints the result to the Serial Monitor.
+  Graphical representation is available using Serial Plotter (Tools > Serial Plotter menu).
+  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
+
+  This example code is in the public domain.
+
+  https://www.arduino.cc/en/Tutorial/BuiltInExamples/ReadAnalogVoltage
+*/
+
+#include <Wire.h>
+#include <LiquidCrystal.h>
+
+
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input on analog pin 0:
+  int sensorValue = analogRead(A0);
+  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+  float voltage = sensorValue * (5.0 / 1023.0);
+  float temperature = voltage * 100;
+  // print out the value you read:
+  Serial.println("Voltage: ");
+  Serial.println(voltage);
+  Serial.println("Temprature: ");
+  Serial.println(temperature);
+  delay(500);
+}
