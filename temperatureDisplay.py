c55ac86fd8617514370223e07b5c5530780f4be3
Bis Mishra <bismishra979@gmail.com>
2023-09-30 08:38:06 -0900

    Functional commit with proper drivers

--- temperatureDisplay.py
+++ temperatureDisplay.py
@@ -1,10 +1,12 @@
-import Drivers
+import sys
+from lcd import drivers
 
-display = Drivers.Lcd()
+display = drivers.Lcd()
 
 try:
     while True: 
         display.lcd_display_string("Temperature: ", 1)
+        display.lcd_display_string("Humidity: ", 2)
 except KeyboardInterrupt:
     print("Clearning up!")
     display.lcd_clear()
